309ae947ef81c598ba2b22396ae6628ec71b196c
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-10-18 12:02:21 +0600

    initial(all): connect to mongoDB, create user with TelegramID, show all questions

---
+++ main.py
@@ -0,0 +1,173 @@
+import telebot
+
+import config
+from pymongo import MongoClient
+
+
+bot = telebot.TeleBot(config.bot_token)
+
+
+class DataBase:
+    def __init__(self):
+        cluster = MongoClient(config.mongo_client)
+
+        self.db = cluster["PMGQuizTelegramBotDB"]
+        self.users = self.db["Users"]
+        self.questions = self.db["Questions"]
+
+        self.questions_count = len(list(self.questions.find({})))
+
+    def get_user(self, chat_id, first_name, last_name, username):
+        user = self.users.find_one({"chat_id": chat_id})
+
+        if user is not None:
+            return user
+
+        user = {
+            "chat_id": chat_id,
+            "first_name": first_name,
+            "last_name": last_name,
+            "username": username,
+            "is_passing": False,
+            "is_passed": False,
+            "question_index": None,
+            "answers": []
+        }
+
+        self.users.insert_one(user)
+
+        return user
+
+    def set_user(self, chat_id, first_name, last_name, username, update):
+        self.users.update_one({"chat_id": chat_id}, {"$set": update})
+        self.users.update_one({"first_name": first_name}, {"$set": update})
+        self.users.update_one({"last_name": last_name}, {"$set": update})
+        self.users.update_one({"username": username}, {"$set": update})
+
+    def get_question(self, index):
+        return self.questions.find_one({"id": index})
+
+
+db = DataBase()
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    user = db.get_user(message.chat.id, message.chat.first_name, message.chat.last_name, message.chat.username)
+
+    if user["is_passed"]:
+        bot.send_message(message.from_user.id, "Vy uzhe otvetily na voprosy. Vtoroy raz nellzya 😥")
+        return
+
+    if user["is_passing"]:
+        return
+
+    db.set_user(message.chat.id, message.chat.first_name, message.chat.last_name, message.chat.username, {"question_index": 0, "is_passing": True})
+
+    user = db.get_user(message.chat.id, message.chat.first_name, message.chat.last_name, message.chat.username)
+    post = get_question_message(user)
+    if post is not None:
+        bot.send_message(message.from_user.id, post["text"], reply_markup=post["keyboard"])
+
+
+@bot.callback_query_handler(func=lambda query: query.data.startswith("?ans"))
+def answered(query):
+    user = db.get_user(query.message.chat.id, query.message.chat.first_name, query.message.chat.last_name, query.message.chat.username)
+
+    if user["is_passed"] or not user["is_passing"]:
+        return
+
+    user["answers"].append(int(query.data.split("&")[1]))
+    db.set_user(query.message.chat.id, query.message.chat.first_name, query.message.chat.last_name, query.message.chat.username, {"answers": user["answers"]})
+
+    post = get_answered_message(user)
+    if post is not None:
+        bot.edit_message_text(post["text"], query.message.chat.id, query.message.id,
+                         reply_markup=post["keyboard"])
+
+
+@bot.callback_query_handler(func=lambda query: query.data == "?next")
+def next(query):
+    user = db.get_user(query.message.chat.id, query.message.chat.first_name, query.message.chat.last_name, query.message.chat.username)
+
+    if user["is_passed"] or not user["is_passing"]:
+        return
+
+    user["question_index"] += 1
+    db.set_user(query.message.chat.id, query.message.chat.first_name, query.message.chat.last_name, query.message.chat.username, {"question_index": user["question_index"]})
+
+    post = get_question_message(user)
+    if post is not None:
+        bot.edit_message_text(post["text"], query.message.chat.id, query.message.id,
+                         reply_markup=post["keyboard"])
+
+
+def get_question_message(user):
+    if user["question_index"] == db.questions_count:
+        count = 0
+        for question_index, question in enumerate(db.questions.find({})):
+            if question["correct"] == user["answers"][question_index]:
+                count += 1
+        percents = round(100 * count / db.questions_count)
+
+        if percents < 40:
+            smile = "😥"
+        elif percents < 60:
+            smile = "😐"
+        elif percents < 90:
+            smile = "😀"
+        else:
+            smile = "😎"
+
+        text = f"Vy otvetili pravillno na {percents}% voprosov {smile}"
+
+        db.set_user(user["chat_id"], user["first_name"], user["last_name"], user["username"], {"is_passed": True, "is_passing": False})
+
+        return {
+            "text": text,
+            "keyboard": None
+        }
+
+    question = db.get_question(user["question_index"])
+
+    if question is None:
+        return
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    for answer_index, answer in enumerate(question["answers"]):
+        keyboard.row(telebot.types.InlineKeyboardButton(f"{chr(answer_index + 97)}) {answer}",
+                                                        callback_data=f"?ans&{answer_index}"))
+
+    text = f"Vopros №{user['question_index'] + 1}\n\n{question['text']}"
+
+    return {
+        "text": text,
+        "keyboard": keyboard
+    }
+
+
+def get_answered_message(user):
+    question = db.get_question(user["question_index"])
+
+    text = f"Vopros №{user['question_index'] + 1}\n\n{question['text']}\n"
+
+    for answer_index, answer in enumerate(question["answers"]):
+        text += f"{chr(answer_index + 97)}) {answer}"
+
+        if answer_index == question["correct"]:
+            text += " ✅"
+        elif answer_index == user["answers"][-1]:
+            text += " ❌"
+
+        text += "\n"
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(telebot.types.InlineKeyboardButton("Dalee", callback_data="?next"))
+
+    return {
+        "text": text,
+        "keyboard": keyboard
+    }
+
+
+bot.polling()
